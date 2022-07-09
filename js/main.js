e498904829f5fed7c324386a63c782553e23c682
Artur Krivenko <88277246+KrivenkoArtur@users.noreply.github.com>
2022-07-09 17:26:24 +0300

    fix design

--- js/main.js
+++ js/main.js
@@ -1,3 +1,48 @@
+
+const txt1 = [
+    'To-Do List.'
+];
+
+function typeText() {
+    let line = 0;
+    let count = 0;
+    let out = '';
+    let htmlOut = document.querySelector('.title');
+
+    function typeLine() {
+        let interval = setTimeout(function() {
+            out += txt1[line][count];
+            htmlOut.innerHTML = out + '|';
+            count++;
+
+            // Проверки
+
+            if (count >= txt1[line].length) {
+                count = 0;
+                line++;
+                if (line == txt1.length) {
+                    clearTimeout(interval);
+                    htmlOut.innerHTML = out;
+                    return true;
+                }
+            }
+
+            typeLine()
+
+        }, getRandomInt(350));
+    }
+
+    typeLine();
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+typeText();
+
+
+
 var nodeList = document.getElementsByTagName('LI')
 var i;
 var close = document.getElementsByClassName("close");
@@ -5,7 +50,7 @@ var i;
 
 for (i = 0; i < nodeList.length; i++) {
     var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
+    var txt = document.createTextNode("e5cd");
     span.className = "close";
     span.appendChild(txt);
     nodeList[i].appendChild(span);
